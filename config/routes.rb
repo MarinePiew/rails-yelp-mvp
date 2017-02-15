f66756d39c2060c44b6ec5fe1aed149833466c2a
macbook <petitm31@gmail.com>
2017-02-15 12:33:30 +0100

    Add model reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
+
+
+
